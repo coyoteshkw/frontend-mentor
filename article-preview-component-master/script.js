cc0c2f777e55c5ead1b97249ba175354271a62f0
coyoteshkw <1944301179@qq.com>
2025-02-15 16:59:00 +0800

    article preview component done.

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -6,13 +6,10 @@ console.log("---");
 console.log(shareBoard);
 
 shareBtn.addEventListener("click", (event) => {
-  console.log("hello");
-  // shareBoard.style.translate = "translateY(0)";
   event.stopPropagation();
   shareBoard.classList.add("up");
 });
 
 card.addEventListener("click", () => {
-  // console.log(card);
   shareBoard.classList.remove("up");
 });
